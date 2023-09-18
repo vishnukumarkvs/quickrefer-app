62a543e25a58226d39a4a370abce1da550b39305
kvssankar <kvs.sankar001@gmail.com>
2023-09-18 14:53:44 +0530

    aadded margin bottom

--- src/components/chat/Messages.jsx
+++ src/components/chat/Messages.jsx
@@ -128,7 +128,7 @@ const Messages = ({ userId, friendId, chatId }) => {
             );
           })}
       </div>
-      <div className="border-t border-gray-200 px-4 pt-4 mb-2 sm:mb-0">
+      <div className="border-t border-gray-200 px-4 pt-4 mb-6 ">
         <div className="relative flex-1 overflow-hidden rounded-lg shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-indigo-600">
           <TextAreaAutosize
             ref={textareaRef}
