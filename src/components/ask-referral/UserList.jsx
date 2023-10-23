8a954aafee66847d7c8be491281517a90f319ca0
kvssankar <kvs.sankar001@gmail.com>
2023-10-23 17:06:25 +0530

    done

--- src/components/ask-referral/UserList.jsx
+++ src/components/ask-referral/UserList.jsx
@@ -4,35 +4,116 @@ import { ResponsiveTable } from "responsive-table-react";
 import Link from "next/link";
 import AddFriendButton from "../chat/AddFriendButton";
 import EmptyComponent from "../emptystates/EmptyComponent";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Flex,
+  Text,
+  IconButton,
+  ButtonGroup,
+  Button,
+  Box,
+  Heading,
+} from "@chakra-ui/react";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { useRouter } from "next/navigation";
+
+const isMobile = () => {
+  try {
+    return window.innerWidth < 768;
+  } catch {
+    return false;
+  }
+};
 
 const UserList = ({ users, url }) => {
-  const columns = [
-    { id: "fullname", text: "Fullname" },
-    { id: "profile", text: "Profile" },
-    { id: "experience", text: "Experience" },
-    { id: "send", text: "Send" },
-  ];
-
-  const data = (users ? users : []).map((user) => ({
-    fullname: user.fullname,
-    profile: (
-      <Link
-        href={`/user/${user.username}`}
-        target="_blank"
-        rel="noopener noreferrer"
-        className="text-blue-500"
-      >
-        {user.username}
-      </Link>
-    ),
-    experience: `${user.experience} yrs`,
-    send: <AddFriendButton id={user.userId} url={url} />,
-  }));
-
+  const router = useRouter();
+  console.log(users);
   return users?.length > 0 ? (
-    <div className="my-5">
-      <ResponsiveTable columns={columns} data={data} />
-    </div>
+    isMobile() ? (
+      <div className="flex flex-col gap-3 my-5 items-start">
+        {users.map((user, index) => (
+          <Card w="full" key={index}>
+            <CardHeader pb="1">
+              <Flex spacing="4">
+                <Flex flex="1" gap="4" alignItems="center" flexWrap="wrap">
+                  <Box>
+                    <Heading size="sm">{user.username}</Heading>
+                    <Text>{user.experience}&nbsp;years</Text>
+                  </Box>
+                </Flex>
+              </Flex>
+            </CardHeader>
+            <CardBody py="0">
+              <Text>{`Works as a ${user.jobRole || "Software Engineer"}`}</Text>
+            </CardBody>
+
+            <CardFooter pt="2">
+              <ButtonGroup spacing="2">
+                <AddFriendButton id={user.userId} url={url} />
+                <Button
+                  size="xs"
+                  onClick={() => {
+                    router.push(`/user/${user.username}`);
+                  }}
+                  variant="ghost"
+                  colorScheme="blue"
+                >
+                  View Profile
+                </Button>
+              </ButtonGroup>
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
+    ) : (
+      <div className="my-5">
+        <Table>
+          <TableCaption>A list of potential referrers.</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[100px]">No</TableHead>
+              <TableHead>Fullname</TableHead>
+              <TableHead>Profile</TableHead>
+              <TableHead>Experience</TableHead>
+              <TableHead className="text-right">Send</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {users.map((user, index) => (
+              <TableRow key={index}>
+                <TableCell>{index + 1}</TableCell>
+                <TableCell>{user.fullname}</TableCell>
+                <TableCell>
+                  <Link
+                    href={`/user/${user.username}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-blue-500"
+                  >
+                    {user.username}
+                  </Link>
+                </TableCell>
+                <TableCell>{user.experience} yrs</TableCell>
+                <TableCell className="text-right">
+                  <AddFriendButton id={user.userId} url={url} />
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+    )
   ) : (
     users && users.length === 0 && (
       <EmptyComponent title="We are onboarding referers from 150+ companies very soon, please wait or try for different company." />
