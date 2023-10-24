4ce971b3ab3211cec915561582eff232c9390c98
kvssankar <kvs.sankar001@gmail.com>
2023-10-24 15:41:33 +0530

    fixed manifest issue pwa

--- src/app/layout.js
+++ src/app/layout.js
@@ -34,27 +34,24 @@ export default function RootLayout({ children }) {
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href="/favicon/apple-touch-icon.png"
+          href="/apple-touch-icon.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href="/favicon/favicon-32x32.png"
+          href="/favicon-32x32.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href="/favicon/favicon-16x16.png"
+          href="/favicon-16x16.png"
         />
-        <link rel="manifest" href="/favicon/site.webmanifest.json" />
-        <link rel="shortcut icon" href="/favicon/favicon.ico" />
-        <meta
-          property="og:image"
-          content={"/favicon/android-chrome-512x512.png"}
-        />
-        <meta name="theme-color" content="#ffffff" />
+        <link rel="manifest" href="/site.webmanifest.json" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <meta property="og:image" content={"/android-chrome-512x512.png"} />
+        <meta name="theme-color" content="#ffcd18" />
       </head>
       <body className={`bg-[#fbf9f0]`}>
         <Provider>{children}</Provider>
